1e04e5a31775ea0de360c2e0900c55752558efee
SalahHassan202 <salahhassan66190@gmail.com>
2026-05-07 05:30:15 +0300

    added google services.json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:trackify_app/features/splash_screen/splash_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:practical_google_maps_example/core/di/dependancy_injection.dart';
+import 'package:practical_google_maps_example/core/routing/router_generation_config.dart';
+import 'package:practical_google_maps_example/core/styling/theme_data.dart';
+import 'package:practical_google_maps_example/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  setUpServiceLocator();
   runApp(const MyApp());
 }
 
@@ -10,6 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const SplashScreen());
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (context, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Order Tracking',
+          theme: AppThemes.lightTheme,
+          routerConfig: RouterGenerationConfig.goRouter,
+        );
+      },
+    );
   }
 }
