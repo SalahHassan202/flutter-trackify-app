db15de9aab566675ee9d074e95d56f3dc2a12bef
SalahHassan202 <salahhassan66190@gmail.com>
2026-05-07 03:09:54 +0300

    update router generation config

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:trackify_app/core/routing/app_routes.dart';
+import 'package:trackify_app/features/add_order_screen/cubit/orders_cubit.dart';
+import 'package:trackify_app/features/add_order_screen/screens/add_order_screen.dart';
 import 'package:trackify_app/features/auth/cubit/auth_cubit.dart';
 import 'package:trackify_app/features/auth/screns/login_screen.dart';
 import 'package:trackify_app/features/auth/screns/register_screen.dart';
@@ -36,6 +38,14 @@ class RouterGenerationConfig {
         name: AppRoutes.homeScreen,
         path: AppRoutes.homeScreen,
         builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        name: AppRoutes.addOrderScreen,
+        path: AppRoutes.addOrderScreen,
+        builder: (context, state) => BlocProvider(
+          create: (context) => sl<OrdersCubit>(),
+          child: AddOrderScreen(),
+        ),
       ),
     ],
   );
